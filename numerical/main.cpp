299bbe717d4632864d9dc332bba3636613c0e3a5
Ilyab <an1mepacan@yandex.ru>
2023-10-06 12:20:10 +0300

    done matrix inversing method

--- numerical/main.cpp
+++ numerical/main.cpp
@@ -3,14 +3,10 @@
 #include "integrate_methods.h"
 #include "matrix_class.h"
 #include <cmath>
+
 using namespace std;
 using namespace biv;
 int main() {
-	Matrix A = { {1, 1, 1}, {1, 2, 3}, {1,4,9} };
-	Matrix b = { 2 * sqrt(2), 4 / 3 * sqrt(2), 8 / 5 * sqrt(2) };
-	b = b.transpose();
-	Matrix ans = GaussSlau(A, b);
-	cout << ans;
-	
-
+	Matrix<double> A = { {1, -1}, {2, 4} };
+	A = A.inverse();
 }
--- numerical/matrix_class.h
+++ numerical/matrix_class.h
@@ -4,41 +4,49 @@
 #include <vector>
 using namespace std;
 namespace biv {
+	template <typename Number>
 	class Matrix;
-	ostream& operator<<(ostream& os, const Matrix& a);
+	template <typename Number>
+	ostream& operator<<(ostream& os, const Matrix<Number>& a);
 
+	template <typename Number>
 	class Matrix {
-		using num = double;
 	public:
-		vector<vector<num>> v;
+		vector<vector<Number>> v;
 		size_t n;
 		size_t m;
 
 		Matrix() : n(0), m(0) {}
-		Matrix(const vector<vector<num>>& v)
+		Matrix(const vector<vector<Number>>& v)
 			: v(v)
 			, n(v.size())
 			, m(v[0].size()) {}
-		Matrix(const vector<num>& v) : n(1), m(v.size()) {
-			vector<vector<num>> copy(1, vector<num>(v.size()));
+		Matrix(const vector<Number>& v) : n(1), m(v.size()) {
+			vector<vector<Number>> copy(1, vector<Number>(v.size()));
 			copy[0] = v;
 			this->v = copy;
 		}
-		Matrix(const initializer_list<initializer_list<num>>& init) {
+		Matrix(const initializer_list<initializer_list<Number>>& init) {
 			n = init.size();
 			m = (*init.begin()).size();
 			for (auto data : init) {
 				v.push_back(data);
 			}
 		}
-		Matrix(const initializer_list<num>& init)
+		Matrix(const initializer_list<Number>& init)
 			: v{ init }
 			, n(1)
 			, m(init.size()) {}
 		Matrix(size_t n, size_t m)
 			: n(n)
 			, m(m) {
-			vector<vector<num>> q(n, vector<num>(m));
+			vector<vector<Number>> q(n, vector<Number>(m));
+			v = q;
+		}
+		Matrix(size_t n, size_t m, Number t)
+			: n(n)
+			, m(m) {
+			vector<vector<Number>> q(n, vector<Number>(m, t));
 			v = q;
 		}
 		//copy constr
@@ -47,7 +55,7 @@ namespace biv {
 			m = a.m;
 			v = a.v;
 		}
-		num& operator()(int i, int j) {
+		Number& operator()(int i, int j) {
 			if (i >= this->n || j >= this->m) {
 				cout << "Seg Fault\n";
 				throw;
@@ -55,7 +63,7 @@ namespace biv {
 			return this->v[i][j];
 		}
 
-		const num& operator()(int i, int j) const {
+		const Number& operator()(int i, int j) const {
 			if (i >= this->n || j >= this->m) {
 				cout << "Seg Fault\n";
 				throw;
@@ -63,14 +71,14 @@ namespace biv {
 			return this->v[i][j];
 		}
 		//Multiply by number
-		Matrix operator*(double x) {
+		Matrix operator*(Number x) {
 			Matrix m = this->v;
 			for (auto& i : m.v)
 				for (auto& j : i) j *= x;
 			return m;
 		}
 
-		Matrix& operator*=(double x) {
+		Matrix& operator*=(Number x) {
 			Matrix m = this->v;
 			for (auto& i : this->v)
 				for (auto& j : i) j *= x;
@@ -89,7 +97,7 @@ namespace biv {
 				cout << "Bad dimensions in multiplying\n";
 				return b;
 			}
-			double curr_sum = 0;
+			Number curr_sum = 0;
 			Matrix a = this->v;
 			Matrix res(a.n, b.m);
 			for (int i = 0; i < a.n; i++) {
@@ -109,8 +117,29 @@ namespace biv {
 					b.v[i][j] = a.v[j][i];
 			return b;
 		}
+		Matrix inverse() {
+			Matrix<Number>& A = *this;
+			if (A.n != A.m) {
+				cout << "Matrix hasnt inverse\n";
+				throw;
+			}
+			Matrix<Number> ANS(A.n, A.m, static_cast<Number>(0));
+
+			for (int i = 0; i < A.n; i++) {
+				Matrix<Number> B(A.n, 1, static_cast<Number>(0));
+				B(i, 0) = 1;
+				Matrix<Number> ans = GaussSlau(A, B);
+				for (int j = 0; j < A.n; j++) {
+					ANS(j, i) = ans(j, 0);
+				}
+			}
+			return ANS;
+
+
+		}
 	};
-	ostream& operator<<(ostream& os, const Matrix& a) {
+	template <typename Number>
+	ostream& operator<<(ostream& os, const Matrix<Number>& a) {
 		for (auto i : a.v) {
 			for (double j : i)
 				os << j << " ";
@@ -118,38 +147,38 @@ namespace biv {
 		}
 		return os;
 	}
-
-	Matrix GaussSlau(const Matrix& A, const Matrix& b) {
+	template <typename Number>
+	Matrix<Number> GaussSlau(const Matrix<Number>& A, const Matrix<Number>& b) {
 		size_t n = b.n;
 		if (A.m != A.n) {
 			cout << "Matrix A is not a square!\n";
 			throw;
 		}
-		Matrix W(n, n + 1);
+		Matrix<Number> W(n, n + 1);
 		for (int i = 0; i < n; i++) {
 			copy((A.v[i]).begin(), (A.v[i]).end(), (W.v[i]).begin());
 			W.v[i][n] = b.v[i][0];
 		}
-		for (size_t j = 0; j < n; j++) {
+		for (int j = 0; j < n; j++) {
 			int maxx = j;
-			for (size_t i = j + 1; i < n; i++)
+			for (int i = j + 1; i < n; i++)
 				if (abs(W(maxx, j)) > abs(W(i, j))) {
 					swap(W.v[maxx], W.v[i]);
 					maxx = i;
 				}
 			double denom = W(j, j);
-			for (size_t i = 0; i < n + 1; i++) {
+			for (int i = 0; i < n + 1; i++) {
 				W(j, i) = W(j, i) * 1.0 / denom;
 			}
-			for (size_t i = j + 1; i < n; i++) {
+			for (int i = j + 1; i < n; i++) {
 				denom = W(i, j);
-				for (size_t q = 0; q < n + 1; q++) {
+				for (int q = 0; q < n + 1; q++) {
 					W(i, q) = W(i, q) - (W(j, q) * denom);
 				}
 			}
 		}
 		// inverse
-		for (int j = n - 1; j > 0; j--)
+		for (int j = static_cast<int>(n) - 1; j > 0; j--)
 		{
 			//for (int dd = 0; dd < n; dd++)
 			//	W.v[j][dd] /= W.v[j][j];
@@ -160,7 +189,7 @@ namespace biv {
 					W(i, dd) = W(i, dd) - (W(j, dd) * denom);
 			}
 		}
-		Matrix res(n, 1);
+		Matrix<Number> res(n, 1);
 		for (size_t i = 0; i < n; i++)
 			res.v[i][0] = W.v[i][n];
 
