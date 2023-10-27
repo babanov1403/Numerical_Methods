e4a4751d77bb0cb154819460077c6cbd7cb39823
Ilyab <an1mepacan@yandex.ru>
2023-10-27 13:34:28 +0300

    alglib

--- numerical/main.cpp
+++ numerical/main.cpp
@@ -8,32 +8,93 @@
 #include "burger.h"
 #include "polynom_class.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "optimization.h"
 
 
 using namespace biv;
 using namespace std;
 int main() {
-	Matrix<double> C_res = buildC(t0, t1, N, A, B, H, g, x0);
+	Matrix<double> RawC = buildC(t0, t1, N, A, B, H, g, x0);
 	cout << "C:\n";
-	cout << C_res;
-	Matrix<double> D_res = buildD(t0, t1, N, A, B, H, g, x0);
+	cout << RawC;
+	Matrix<double> RawD = buildD(t0, t1, N, A, B, H, g, x0);
 	cout << "D:\n";
-	cout << D_res;
+	cout << RawD;
 	cout << "g0:\n";
-	cout << compute_g0(g, H, x0);
+	Matrix<double> RawG0  = compute_g0(g, H, x0);
+	cout << RawG0;
+	//у нас есть вектор C 1xN, вектор D mxN, g0 mx1, и граниченые условия |u_i| < 1 =>>
 
+	alglib::real_2d_array D; D.setlength(H.n, N);
+	alglib::real_1d_array Target; Target.setlength(N);
+	alglib::real_1d_array LBoundaries_u; LBoundaries_u.setlength(N);
+	alglib::real_1d_array UBoundaries_u; UBoundaries_u.setlength(N);
+	alglib::real_1d_array LBoundaries_D; LBoundaries_D.setlength(H.n);
+	alglib::real_1d_array UBoundaries_D; UBoundaries_D.setlength(H.n);
+	alglib::real_1d_array Scale; Scale.setlength(N);
+	alglib::minlpstate state;
+	alglib::minlpreport rep;
+	alglib::real_1d_array x;
+	alglib::minlpcreate(N, state);
+	//filling arrays
+	for (int i = 0; i < RawD.n; i++) 
+		for (int j = 0; j < RawD.m; j++)
+			D(i, j) = RawD(i, j);
+	
+	for (int i = 0; i < RawD.n; i++){
+		for (int j = 0; j < RawD.m; j++)
+			cout << D(i, j) << ' ';
+		cout << "\n";
+	}
 
+	for (int i = 0; i < N; i++)
+		Target(i) = -RawC(0, i);
+	
+	for (int i = 0; i < N; i++)
+		cout << Target(i) << ' ';
+	cout << '\n';
+
+	for (int i = 0; i < N; i++) {
+		LBoundaries_u(i) = -1;
+		UBoundaries_u(i) = 1;
+	} 
+
+	for (int i = 0; i < N; i++) {
+		cout << LBoundaries_u(i)  << ' ';
+		cout << UBoundaries_u(i)  << ' ';
+		cout << '\n';
+	}
+
+	for (int i = 0; i < H.n; i++) {
+		LBoundaries_D(i) = RawG0(i, 0);
+		UBoundaries_D(i) = RawG0(i, 0);
+	}
+	for (int i = 0; i < N; i++) {
+		cout << LBoundaries_D(i) << ' ';
+		cout << UBoundaries_D(i) << ' ';
+		cout << '\n';
+	}
+
+	for (int i = 0; i < N; i++)
+		Scale(i) = 1;
+	//////
+	alglib::minlpsetcost(state, Target);
+	alglib::minlpsetbc(state, LBoundaries_u, UBoundaries_u);
+	alglib::minlpsetlc2dense(state, D, LBoundaries_D, UBoundaries_D);
+	alglib::minlpsetscale(state, Scale);
+
+	//solving
+	alglib::minlpoptimize(state);
+	alglib::minlpresults(state, x, rep);
+	cout << rep.terminationtype << '\n';
+	cout << "x:\n";
+	for (int i = 0; i < N; i++) cout << x(i) << ' ';
+	cout << "Target:\n";
+	double ans = 0;
+	for (int i = 0; i < N; i++) 
+		ans += Target(i) * x(i);
+	cout << ans << '\n';
 }
-//Matrix<double> D_res = buildD(t0, t1, N, A, B, H, g, x0);
-	//cout << "D:\n";
-	//cout << D_res;
-	/*vector<double> nodes = makeNodes(27);
-	double integrale = computeIntegral(makeISF(nodes), nodes);
-	cout << setprecision(11) << integrale << " ";*/
-
-	//cout << "g0:\n";
-	//Matrix<double> g0 = compute_g0(g, H, compute_x0(A, x0, t1, t0));
-	//cout << g0;
-	//cout << "G:\n";
-	//Matrix<double> G = compute_G(A, H, t0, t1);
-	//cout << G;
