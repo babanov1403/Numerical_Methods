635e8d49f0005cb068315af57c0533799b9c47a7
Ilyab <an1mepacan@yandex.ru>
2023-12-16 15:53:52 +0300

    123

--- numerical/main.cpp
+++ numerical/main.cpp
@@ -17,17 +17,17 @@
 using namespace biv;
 using namespace std;
 int main() {
-	alglib::real_2d_array D = "[[-2, -1, 2, 0, 0], [1, 1, 4, 1, 3], [3, 1, -1, 0, 6]]";
-	alglib::real_1d_array Target = "[0, 1, -3, -1, -1]";
-	alglib::real_1d_array LBoundaries_u = "[0, 0, 0, 0, 0]";
-	alglib::real_1d_array UBoundaries_u = "[+inf, +inf, +inf, +inf, +inf]";
-	alglib::real_1d_array LBoundaries_D = "[2, 8, 5]";
-	alglib::real_1d_array UBoundaries_D = "[2, 8, 5]";
-	alglib::real_1d_array Scale = "[1, 1, 1, 1, 1]";
+	alglib::real_2d_array D = "[[3, 4, 1], [3, 2, 1]]";
+	alglib::real_1d_array Target = "[5, 1, 2]";
+	alglib::real_1d_array LBoundaries_u = "[0, -inf, 0]";
+	alglib::real_1d_array UBoundaries_u = "[+inf, +inf, +inf]";
+	alglib::real_1d_array LBoundaries_D = "[12, 6]";
+	alglib::real_1d_array UBoundaries_D = "[12, +inf]";
+	alglib::real_1d_array Scale = "[1, 1, 1]";
 	alglib::minlpstate state;
 	alglib::minlpreport rep;
 	alglib::real_1d_array x;
-	alglib::minlpcreate(5, state);
+	alglib::minlpcreate(3, state);
 	
 	alglib::minlpsetcost(state, Target);
 	alglib::minlpsetbc(state, LBoundaries_u, UBoundaries_u);
@@ -40,6 +40,6 @@ int main() {
 	cout << "State:\n";
 	cout << rep.terminationtype << '\n';
 	cout << "x:\n";
-	for (int i = 0; i < 5; i++) cout << x(i) << ' ';
+	for (int i = 0; i < 3; i++) cout << x(i) << ' ';
 
 }
